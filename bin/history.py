399e12d68677e029659b15779130003d075a3618
Jarmo <nurse.jason@gmail.com>
2015-10-17 01:01:30 -0400

    Added todo board as a big comment

--- bin/history.py
+++ bin/history.py
@@ -3,6 +3,45 @@
 A simple script to call the Steam API, given a sequence number, to retrieve
 match history and save it to a database.
 """
+
+""" TODO Board
+1. Add logging
+2. Continuous running
+<- This file should be done!
+-> Begin Grabbing Basic Hero Data
+3. DB For all heroes: name, id, localized_name, win%
+4. Script to save hero data to DB
+<- Heroes should be done
+-> Begin data relation script
+5. DB For win_loss_relation:
+    hero_id, compare_hero_id, win%
+6. for hero in heroes:
+    a. SELECT losing_hero_1-5 FROM hero_matches WHERE winning_hero_1-5 = hero
+    b. for match in result:
+        i. add to dict
+    c. add to overall dict
+save to db
+<- data relation script complete
+-> Begin writing UI component -- Main Logic -- API?
+7. for each hero picked, grab win rates of all remaining heroes.
+   average all win rates together
+   return averaged win rates
+    SELECT * from win_loss_relation
+    WHERE hero_id or compare_hero_id IN [heroes_picked]
+<- End main logic, make pretty GUI
+----->
+BASIC APPLICATION MILESTONE COMPLETE
+At this point some minor clean arch refactoring should take place.
+This should remain a 'behemoth' application for the time being.
+Will need to consider where to go next from here. Should I begin adding ad
+revenue and working on SEO? Or should I start working on the more advanced
+pieces that will make this more accurate.
+<----
+# One thing worth noting... Should supports or various hero types carry
+different weights? Or do all heroes have equal impact on the game? I think all
+have equal impact.
+"""
+
 import logging # TODO
 import requests
 from sqlalchemy import create_engine
