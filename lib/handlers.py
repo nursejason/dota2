8f38bd23bb9cd91312840bf570d6c98d5e4d76f8
Jarmo <nurse.jason@gmail.com>
2015-12-31 01:03:57 -0500

    Consume domain logic from app layer

--- lib/handlers.py
+++ lib/handlers.py
@@ -1,9 +1,18 @@
 """ App logic """
 import os
 import sys
+
 sys.path.insert(0, os.path.realpath(os.path.dirname(__file__)) + '/../lib')
 
-import domain
+from domain import generate_match_relations
 
-def process_data():
-    pass
+def process_matches_by_sequence_num(sequence_num, interactors):
+    result = interactors.api_interactor.call_steam(sequence_num)
+    relations = generate_match_relations(result['matches'])
+    interactors.storage_interactor.save_relations(relations)
+
+def get_sequence_number(storage_interactor):
+    return storage_interactor.get_sequence_number()
+
+def set_sequence_number(storage_interactor, sequence_number):
+    storage_interactor.set_sequence_number(sequence_number)
